d1a28b46894df55142b8c3e1190d0f56b89b3974
Artyom Zolotoverkhov <artyomzolotoverkhov@gmail.com>
2020-04-24 21:33:31 +0300

    Added UnitOfMeasure class and realized OneToOne ralationship.

---
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -0,0 +1,48 @@
+package guru.springframework.domain;
+
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
+
+    private BigDecimal amount;
+
+    public Ingredient(String description) {
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
+    }
+}
